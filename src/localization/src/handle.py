1fb1283ce84d38a3bf56890da9cc38e5df58127c
jormunrod <jormunrod@alum.us.es>
2022-12-07 19:49:01 +0100

    Add GPS Position & Acceleration to localization node

--- src/localization/src/handle.py
+++ src/localization/src/handle.py
@@ -3,10 +3,8 @@ import tf
 import tf2_ros
 import geometry_msgs.msg
 
-from sbg_driver.msg import SbgGpsPos, SbgEkfEuler, SbgImuData, SbgGpsVel
-from sbg_driver.msg import SbgMag
-from sbg_driver.msg import SbgEkfNav
-from common_msgs.msg import velState #,GpsPos
+from sbg_driver.msg import SbgGpsPos, SbgEkfEuler, SbgImuData, SbgGpsVel, SbgMag, SbgEkfNav
+from common_msgs.msg import velState
 
 
 
@@ -22,12 +20,14 @@ class Localization():
 
     def subscribe_topics(self):
         rospy.Subscriber("/sbg/ekf_nav", SbgEkfNav, self.send_velocity)
-        # rospy.Subscriber("/sbg/gps_pos", SbgGpsPos, self.send_position)
+        rospy.Subscriber("/sbg/gps_pos", SbgGpsPos, self.send_position)
+        rospy.Subscriber("/sbg/mag", SbgMag, self.send_acceleration)
 
 
     def publish_topics(self):
-        self.pub = rospy.Publisher("vel_state", velState, queue_size=1)
-        # self.pub = rospy.Publisher("gps position", GpsPos, queue_size=1)
+        self.pub = rospy.Publisher("/vel_state", velState, queue_size=1)
+        self.pub = rospy.Publisher("/gps_position", SbgGpsPos, queue_size=1)
+        self.pub = rospy.Publisher("/acceleration", SbgMag, queue_size=1)
 
 
     def send_velocity(self, msg):
@@ -38,18 +38,15 @@ class Localization():
 
         self.pub.publish(vel)
         
-    #def send_position(self, msg):
-    #    position = GpsPos()
-    #    position.latitude = msg.latitude
-    #    position.longitude = msg.longitude
-    #    position.altitude = msg.altitude
+    def send_position(self, msg):
+        position = SbgGpsPos()
+        position.latitude = msg.latitude
+        position.longitude = msg.longitude
 
-    #    self.pub.publish(position)
+        self.pub.publish(position)
 
+    def send_acceleration(self, msg):
+        mag = SbgMag()
+        mag.accel = msg.acceleration
 
-        #br = tf2_ros.TransforBroadcaster()
-        #t = geometry_msgs.msg.TransformStamped()
-
-        #t.header.stamp = rospy.Time.now() # Marca de tiempo
-        #t.header.frame_id = "position" #Nombre del marco padre
-        #t.child_frame_id = position
+        self.pub.publish(mag)
