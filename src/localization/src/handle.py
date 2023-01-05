1b8cf8b6f2c35b5d1d577ba15ca25a58f9893753
jormunrod <jormunrod@alum.us.es>
2023-01-05 16:25:10 +0100

    Add camera frame

--- src/localization/src/handle.py
+++ src/localization/src/handle.py
@@ -24,11 +24,11 @@ class Localization():
         rospy.Subscriber("/sbg/gps_pos", SbgGpsPos, self.send_position)
         rospy.Subscriber("/sbg/imu_data", SbgImuData, self.send_acceleration)
         rospy.Subscriber("/sbg/gps_vel", SbgGpsVel, self.send_gps_velocity)
-        #TF2 WIP
+
         rospy.Subscriber("/gps_position", Vector3, self.tf2_start_point)
         rospy.Subscriber("/gps_position", Vector3, self.tf2_imu_position)
+        rospy.Subscriber("/gps_position", Vector3, self.tf2_camera_position)
         
-
 
     def publish_topics(self):
         self.pub_vel = rospy.Publisher("/velocity", Vector3, queue_size=1)
@@ -36,10 +36,8 @@ class Localization():
         self.pub_acc = rospy.Publisher("/acceleration", Vector3, queue_size=1)
         self.pub_gps_vel = rospy.Publisher("/gps_velocity", Vector3, queue_size=1)
 
-
     def send_velocity(self, msg):
         vel = Vector3()
-
         vel.x = msg.velocity.x 
         vel.y = msg.velocity.y
         vel.z = msg.velocity.z
@@ -48,7 +46,6 @@ class Localization():
 
     def send_gps_velocity(self, msg):
         gps_vel = Vector3()
-
         gps_vel.x = msg.velocity.x # North
         gps_vel.y = msg.velocity.y # East
         gps_vel.z = msg.velocity.z # Down
@@ -70,7 +67,7 @@ class Localization():
     
         self.pub_acc.publish(acc)
 
-    # TF Implementation. WIP.
+    # TF2 IMPLEMENTATION
     def tf2_start_point(self, msg):
         br = tf2_ros.StaticTransformBroadcaster()
         t = geometry_msgs.msg.TransformStamped()
@@ -100,6 +97,25 @@ class Localization():
         
         t.transform.translation.x = msg.x
         t.transform.translation.y = msg.y
+        t.transform.translation.z = 0.0
+
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
+
+        br.sendTransform(t)
+
+    def tf2_camera_position(self, msg):
+        br = tf2_ros.StaticTransformBroadcaster()
+        t = geometry_msgs.msg.TransformStamped()
+
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = "imu"
+        t.child_frame_id = "camera"
+        
+        t.transform.translation.x = 0.0 # Modificar para que sea la distancia entre el IMU y la camara
+        t.transform.translation.y = 0.0
         t.transform.translation.z = 0.0
 
         t.transform.rotation.x = 0.0
