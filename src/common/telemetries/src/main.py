d1fa160fcf15345876413a33c479c40311d278f5
gxnzalx55 <gxnzalx.55@gmail.com>
2024-04-13 11:14:09 +0200

    telemetriesv2.1

--- src/common/telemetries/src/main.py
+++ src/common/telemetries/src/main.py
@@ -2,9 +2,12 @@
 import rospy
 from writter import DataLoggerNode
 
+sim_mode = rospy.get_param('/control_pure_pursuit/simulation')
+
 def main():
     rospy.init_node('DataLoggerNode', anonymous=True)
-    DataLoggerNode()
+    if sim_mode:
+        DataLoggerNode()
     rospy.spin()
 
 if __name__ == '__main__':
