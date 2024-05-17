674797ea40a657a47a957b1c1a0c1d5b8ba9033f
Alvaro <alplepe02@gmail.com>
2024-05-17 16:08:41 +0200

    break control corrected

--- src/common/tests/src/inspection_control.py
+++ src/common/tests/src/inspection_control.py
@@ -5,21 +5,21 @@ from std_msgs.msg import Float32, Int16
 import math
 import time
 
-DURATION = 20000
 AMPLITUDE = 19
-FREQUENCY = 0.2
+FREQUENCY = 0.1
 #0.2, 0.3, 0.5, 0.1, 0.3
-TARGET_SPEED = 10
+TARGET_SPEED = [3,5]
+STABLE_DURATION = 0.5
 #3.5, 4, 4, 1, 2
 BRAKE_DURATION = 5
 BRAKE_THRESHOLD = 2
 SPEED_MARGIN = 0.05 # 5%
-STABLE_DURATION = 5
+
 
 
 AS_status = 0
 
-kp=0.8
+kp=1
 ki=0
 kd=0
 
@@ -31,10 +31,11 @@ current_time=0
 start_time=0
 prev_time = 0
 prev_error = 0
-speed_within_target_start = 0
 brake_start_time = 0
+target_reached_time=0
 braking = False
-is_speed_stable = False
+
+i=0
 
 
 def AS_status_callback(AS_status_msg: Int16):
@@ -45,7 +46,7 @@ def AS_status_callback(AS_status_msg: Int16):
 
 
 def speed_callback(motor_speed_msg: Float32):
-        global current_time, start_time, AS_status, braking, is_speed_stable, speed_within_target_start, brake_start_time
+        global current_time, start_time, AS_status, braking, brake_start_time, i, target_reached_time
         current_time = time.time()
 
         # Create a Controls message and assign sinusoidal steering and calculated acceleration
@@ -57,29 +58,26 @@ def speed_callback(motor_speed_msg: Float32):
             control_msg.steering = generate_sinusoidal_steering(current_time-start_time)
         # control_msg.steering = 0
 
-        if AS_status==0x02 and not braking:
-            if TARGET_SPEED * (1 - SPEED_MARGIN) <= motor_speed_msg.data <= TARGET_SPEED * (1 + SPEED_MARGIN):
-                if not is_speed_stable:
-                    speed_within_target_start = current_time
-                    is_speed_stable = True
-                elif current_time - speed_within_target_start >= STABLE_DURATION:
-                    braking = True
-                    brake_start_time = rospy.get_time()
-            else:
-                is_speed_stable = False
-        
         if braking:
-            motor_speed_msg.data = brake_control(TARGET_SPEED, BRAKE_THRESHOLD)
-        
-        control_msg.accelerator = accelerator_control(motor_speed_msg.data, TARGET_SPEED)
-        
-        #control_msg.accelerator = 0
+            actual_target = brake_control(TARGET_SPEED[-1],motor_speed_msg.data)
+        else:
+            actual_target = TARGET_SPEED[i]
 
-        # Publish the message
-        #rospy.loginfo(AS_status)
-        if current_time-start_time<DURATION and AS_status==0x02:
-                control_publisher.publish(control_msg)
-                rospy.loginfo(control_msg)
+        if check_target_reached(actual_target,motor_speed_msg.data) and not braking:
+            if (i<len(TARGET_SPEED)-1):
+                i+=1
+                target_reached_time=0
+            else:
+                braking = True
+                brake_start_time=time.time()
+
+
+        
+        control_msg.accelerator = accelerator_control(motor_speed_msg.data, actual_target)
+
+        if AS_status==0x02:
+            control_publisher.publish(control_msg)
+            rospy.loginfo(control_msg)
 
 def generate_sinusoidal_steering(time):
     steering_angle = AMPLITUDE * math.cos(2 * math.pi * FREQUENCY * time)
@@ -89,8 +87,6 @@ def accelerator_control(current: float, target: float):
         global current_time, prev_time, prev_error, Cp, Ci, Cd
 
         error = target - current
-
-
         dt = current_time-prev_time
 
         # Calculate gains
@@ -108,22 +104,39 @@ def accelerator_control(current: float, target: float):
 
         cmd = kp*Cp + ki*Ci + kd*Cd
 
-        return min(cmd, 1) 
+        return max(min(cmd, 1),-1) 
     
-def brake_control(target_speed: float, brake_target_speed: float):
+def brake_control(target_speed: float,vel: float):
     global brake_start_time
     
-    time_since_brake_start = rospy.get_time() - brake_start_time
-    descent_time = BRAKE_DURATION
-    brake_speed = ((brake_target_speed - target_speed) * time_since_brake_start) / descent_time + target_speed
+    brake_target = ((BRAKE_THRESHOLD - target_speed) * (time.time() - brake_start_time))/BRAKE_DURATION + target_speed
+    brake_target = max(brake_target,BRAKE_THRESHOLD)
+
+    if vel < BRAKE_THRESHOLD*(1+SPEED_MARGIN):
+        brake_target = 0
+        msg = Int16()
+        msg.data = 3
+        AS_publisher.publish(msg)
+        
+        # rospy.loginfo("Braking completed, speed below threshold")
+        
+    return brake_target
+
+def check_target_reached(actual_target,vel):
+    global target_reached_time
+    if actual_target*(1 - SPEED_MARGIN) < vel < actual_target*(1 + SPEED_MARGIN):
+        if target_reached_time==0:
+            target_reached_time = time.time()
+        elif time.time()-target_reached_time > STABLE_DURATION:
+            return True
+    else:
+        return False
+
     
-    if brake_speed < BRAKE_THRESHOLD:
-        brake_speed = 0
-        # Añadir mensaje publisher para mandar por CAN el mensaje AS_Status 0x03 en el byte 2
-        
-        rospy.loginfo("Braking completed, speed below threshold")
-        
-    return brake_speed
+
+         
+
+
 
 if __name__ == '__main__':
     rospy.init_node('sinusoidal_control_node')
@@ -133,6 +146,7 @@ if __name__ == '__main__':
     rospy.Subscriber('/can/AS_status', Int16, AS_status_callback,queue_size=1)
 
     control_publisher = rospy.Publisher('/controls', Controls, queue_size=1)
+    AS_publisher = rospy.Publisher('/can/AS_status', Int16, queue_size=1)
     rospy.Subscriber('/motor_speed', Float32, speed_callback,queue_size=1)
 
     rospy.spin()
