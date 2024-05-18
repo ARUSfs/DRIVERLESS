8e9baa4a67919ac8a32f081077075fea76bfee88
Alvaro <alplepe02@gmail.com>
2024-05-18 15:12:58 +0200

    params adjusted

--- src/common/tests/src/inspection_control.py
+++ src/common/tests/src/inspection_control.py
@@ -7,28 +7,28 @@ import time
 
 
 AMPLITUDE = 20
-FREQUENCY = 0.2
-TARGET_SPEED = 0.5
+FREQUENCY = 0.1
+TARGET_SPEED = 0
 
 AS_status = 0
 
 kp=0.5
 
-current_time=0
 prev_time = 0
 prev_error = 0
 
 
 def AS_status_callback(AS_status_msg: Int16):
-        global AS_status
+        global AS_status, start_time
         AS_status = AS_status_msg.data
+        if AS_status == 2 and start_time==0:
+              start_time = time.time()
 
 
 def speed_callback(motor_speed_msg: Float32):
-        current_time = rospy.get_time()
 
         control_msg = Controls()
-        control_msg.steering = generate_sinusoidal_steering(current_time)
+        control_msg.steering = generate_sinusoidal_steering(time.time()-start_time)
         # control_msg.accelerator = accelerator_control(motor_speed_msg.data, TARGET_SPEED)
         control_msg.accelerator = 0
 
@@ -36,7 +36,8 @@ def speed_callback(motor_speed_msg: Float32):
             control_publisher.publish(control_msg)
 
 def generate_sinusoidal_steering(time):
-    steering_angle = AMPLITUDE * math.sin(2 * math.pi * FREQUENCY * time)
+#     steering_angle = AMPLITUDE * math.sin(2*math.pi*FREQUENCY *time)
+    steering_angle = AMPLITUDE * math.sin(FREQUENCY * time*time)
     return steering_angle
 
 def accelerator_control(current: float, target: float):
