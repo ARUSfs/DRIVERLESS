87070a092533b4527e59a6ec68a9d7689e3f8728
Jacobo Pindado Perea <pindado.jacobo@gmail.com>
2024-05-26 20:31:01 +0200

    Finished  class that calculates the pursuit point given the path and current position

--- src/control/PP_cpp/src/PurePursuit.cpp
+++ src/control/PP_cpp/src/PurePursuit.cpp
@@ -2,6 +2,11 @@
 
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <pcl/common/norms.h>
+
+#include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
 
 using namespace std;
 using namespace pcl;
@@ -17,5 +22,67 @@ void PurePursuit::update_path(PointCloud<PointXY>::Ptr new_path) {
     path_updated = true;
 }
 
-PointXY search_pursuit_point(float look_ahead_distance) {
+
+PointXY PurePursuit::search_pursuit_point(const float look_ahead_distance) {
+    // TODO: Should check for empty path in class
+
+    geometry_msgs::TransformStamped transform;
+    PointXY car_position;
+    try {
+        transform = tfBuffer.lookupTransform(car_frame, global_frame, ros::Time(0));
+        car_position.x = transform.transform.translation.x;
+        car_position.y = transform.transform.translation.y;
+    } catch(tf2::TransformException &ex) {
+        car_position = {0.0f, 0.0f};
+        ROS_ERROR("%s", ex.what());
+    }
+
+    size_t closest_point_index = 0;
+    float min_distance = numeric_limits<float>::infinity();
+
+    if(path_updated) {
+        distance_along_curve.clear();
+    }
+
+    for(size_t point_index = 0; point_index < path.size(); point_index++){
+        const PointXY current_point = path[point_index];
+
+        // Actually not the distance but L2 squared, since the actual distance
+        // doesn't matter.
+        const float dx = current_point.x - car_position.x;
+        const float dy = current_point.y - car_position.y;
+        const float current_distance = dx*dx + dy*dy; // Multiplying faster than pow
+
+        if(current_distance < min_distance){
+            min_distance = current_distance;
+            closest_point_index = point_index;
+        }
+
+        // To avoid constantly calculating the length along the curve from
+        // the beginning of it, just do it when the path has changed.
+        // Doing it in this loop to avoid an extra loop.
+        if(path_updated) {
+            if(point_index == 0){
+                distance_along_curve.push_back(0.0f);
+            }
+            else {
+                const PointXY previous_point =  path[point_index-1];
+                const float dist_dx = current_point.x - previous_point.x;
+                const float dist_dy = current_point.y - previous_point.y;
+                const float distance_from_previous = dist_dx*dist_dx + dist_dy*dist_dy;
+                distance_along_curve.push_back(distance_from_previous);
+            }
+        }
+    }
+
+    const float closest_point_dist = distance_along_curve[closest_point_index];
+    for(size_t point_index = closest_point_index; point_index < path.size(); point_index++) {
+        const float cur_dist = distance_along_curve[point_index] - closest_point_dist;
+        if(cur_dist > look_ahead_distance) {
+            return path[point_index];
+        }
+    }
+
+    return path[path.size() - 1];
+
 }
