7a00dc8069f921526c182832a3c73cea39258b97
Alvaro <alplepe02@gmail.com>
2024-05-27 11:19:25 +0200

    can filters activated

--- src/car_interface/can/src/can_reader.py
+++ src/car_interface/can/src/can_reader.py
@@ -26,17 +26,17 @@ class CanReader:
         
         self.bus0 = can.interface.Bus(channel='can0', bustype='socketcan')
         self.bus1 = can.interface.Bus(channel='can1', bustype='socketcan')
-        # self.bus0.set_filters([{"can_id": 0x181, "can_mask": 0x7FF, "extended": False}])
-        # self.bus1.set_filters([{"can_id": 0x182, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x380, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x394, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x392, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x384, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x382, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x185, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x205, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x187, "can_mask": 0x7FF, "extended": False},
-        #                        {"can_id": 0x334, "can_mask": 0x7FF, "extended": False}])
+        self.bus0.set_filters([{"can_id": 0x181, "can_mask": 0x7FF, "extended": False}])
+        self.bus1.set_filters([{"can_id": 0x182, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x380, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x394, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x392, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x384, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x382, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x185, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x205, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x187, "can_mask": 0x7FF, "extended": False},
+                               {"can_id": 0x334, "can_mask": 0x7FF, "extended": False}])
         #--------------------------------------------------------
 
         rospy.Timer(rospy.Duration(1/500), self.publish_IMU)
