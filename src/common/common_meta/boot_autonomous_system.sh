a1a799a9c6ea2296be055f72bf1b00e615aa87da
Ángel <angelgares6424@gmail.com>
2024-07-08 17:00:29 +0200

    no puedo mas

--- src/common/common_meta/boot_autonomous_system.sh
+++ src/common/common_meta/boot_autonomous_system.sh
@@ -3,5 +3,5 @@ source /opt/ros/noetic/setup.bash
 source /home/arus/workspaces/arus_ws/devel/setup.bash
 # Ejecutar launchs
 sleep 5
-#roslaunch common_meta pp_test.launch
+roslaunch can_c mission.launch
 
