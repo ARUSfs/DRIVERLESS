7231e94e1f81bf8a24e30b5c9821bd3fa75cb8ba
Alvaro <alplepe02@gmail.com>
2024-07-16 17:32:09 +0200

    need to adjust params

--- src/localization/ekf_slam/src/ekf_slam.py
+++ src/localization/ekf_slam/src/ekf_slam.py
@@ -10,15 +10,17 @@ import time
 class EKF_SLAM:
     def __init__(self):
 
-        self.R = np.diag([0.002,0.002,0.0005]) # sigma_x, sigma_y, sigma_theta
-        self.Q = np.diag([0.003,0.005]) # sigma_r, sigma_phi
+        # self.R = np.diag([0.002,0.002,0.0005]) # sigma_x, sigma_y, sigma_theta
+        # self.Q = np.diag([0.003,0.005]) # sigma_r, sigma_phi
+        self.R = np.diag([0.00,0.0,0.000]) # sigma_x, sigma_y, sigma_theta
+        self.Q = np.diag([0.00,0.00]) # sigma_r, sigma_phi
         self.Fx = np.eye(3) # Used in both prediction and measurement updates
         self.prev_t = time.time()
 
 
         # Initialize robot state estimate and sigma
         self.mu = np.zeros((3,1)) # px, py, theta
-        self.sigma = np.eye(3)*0.01
+        self.sigma = np.eye(3)*0.000000000000001
         self.sigma[2,2] = 0
     
 
@@ -71,28 +73,31 @@ class EKF_SLAM:
     
 
     def measurement_update(self,map):
-        x = self.mu[0]
-        y = self.mu[1]
-        yaw = self.mu[2] 
+        rospy.loginfo("############################################")
+        x = self.mu[0][0]
+        y = self.mu[1][0]
+        yaw = self.mu[2][0] 
+        # x = 0
+        # y = 0
+        # yaw = 0
         delta_zs = [np.zeros((2,1)) for _ in range(self.n_landmarks)] # A list of how far an actual measurement is from the estimate measurement
         Ks = [np.zeros((self.mu.shape[0],2)) for _ in range(self.n_landmarks)] # A list of matrices stored for use outside the measurement for loop
         Hs = [np.zeros((2,self.mu.shape[0])) for _ in range(self.n_landmarks)] # A list of matrices stored for use outside the measurement for loop
     
         for c in map:
-            dist = np.sqrt((self.mu[0]+c[0])**2 + (self.mu[1]+c[1])**2)
-            phi = self.mu[2] + np.arctan2(c[1],c[0])
+            dist = np.sqrt(c[0]**2 + c[1]**2)
+            phi = np.arctan2(c[1],c[0])
 
-            mu_landmark = np.array([x + dist*np.cos(phi+yaw),
-                                    y+ dist*np.sin(phi+yaw)]) 
+            mu_landmark = np.array([[x + dist*np.cos(phi+yaw)],
+                                    [y+ dist*np.sin(phi+yaw)]]) 
             
             d_min = np.Infinity
             index = None
             for i in range(self.n_landmarks):
-                d = np.linalg.norm(self.mu[i+3:i+5]-mu_landmark)
+                d = np.sqrt((self.mu[i+3,0]-mu_landmark[0,0])**2 + (self.mu[i+4,0]-mu_landmark[1,0])**2)
                 if d < d_min:
                     d_min = d
                     index = i
-                    
             if d_min < self.threshold:
                 mu_landmark = self.mu[index+3:index+5]
             else:
@@ -100,8 +105,8 @@ class EKF_SLAM:
                 self.mu = np.vstack((self.mu, mu_landmark))
                 self.Fx = np.pad(self.Fx, ((0, 0), (0, 2)), 'constant', constant_values=0)
                 self.sigma = np.pad(self.sigma, ((0, 2), (0, 2)), 'constant', constant_values=0)
-                self.sigma[-2,-2] = 0.001
-                self.sigma[-1,-1] = 0.001
+                self.sigma[-2,-2] = 0.0000001
+                self.sigma[-1,-1] = 0.0000001
                 self.n_landmarks+=1
 
                 delta_zs.append(np.zeros((2,1)))
@@ -119,11 +124,12 @@ class EKF_SLAM:
             z_est_arr = np.array([[dist_est],[phi_est]]) # Estimated observation, in numpy array
             z_act_arr = np.array([[dist],[phi]]) # Actual observation in numpy array
             delta_zs[index] = z_act_arr-z_est_arr # Difference between actual and estimated observation
-
+            
             # Helper matrices in computing the measurement update
             Fxj = np.block([[self.Fx],[np.zeros((2,self.Fx.shape[1]))]])
             Fxj[3:5,3+2*index:3+2*index+2] = np.eye(2)
-            H = np.array([[-delta[0,0]/np.sqrt(q),-delta[1,0]/np.sqrt(q),0,delta[0,0]/np.sqrt(q),delta[1,0]/np.sqrt(q)],\
+            
+            H = np.array([[-delta[0,0]/np.sqrt(q),-delta[1,0]/np.sqrt(q),0,delta[0,0]/np.sqrt(q),delta[1,0]/np.sqrt(q)],
                         [delta[1,0]/q,-delta[0,0]/q,-1,-delta[1,0]/q,+delta[0,0]/q]])
             H = H @ Fxj
             Hs[index] = H # Added to list of matrices
@@ -133,11 +139,17 @@ class EKF_SLAM:
         mu_offset = np.zeros(self.mu.shape) # Offset to be added to state estimate
         sigma_factor = np.eye(self.sigma.shape[0]) # Factor to multiply state uncertainty
         for index in range(self.n_landmarks):
+            Ks[index] = np.pad(Ks[index], ((0, self.sigma.shape[0]-Ks[index].shape[0]),(0,0)), 'constant', constant_values=0)
+            Hs[index] = np.pad(Hs[index], ((0,0),(0, self.sigma.shape[0]-Hs[index].shape[1])), 'constant', constant_values=0)
+            
             mu_offset += Ks[index]@delta_zs[index] # Compute full mu offset
             sigma_factor -= Ks[index]@Hs[index] # Compute full sigma factor
             
         self.mu = self.mu + mu_offset # Update state estimate
         self.sigma = sigma_factor @ self.sigma # Update state uncertainty
+
+        rospy.loginfo(self.mu[:3,0])
+        rospy.loginfo(self.mu.shape)
        
 
 
