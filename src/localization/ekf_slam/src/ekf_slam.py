e8beae0ea79df2483e6c048c2bd933bf7402a917
Alvaro <alplepe02@gmail.com>
2024-07-16 14:22:41 +0200

    mu padding working

--- src/localization/ekf_slam/src/ekf_slam.py
+++ src/localization/ekf_slam/src/ekf_slam.py
@@ -4,6 +4,7 @@ from fssim_common.msg import State
 from common_msgs.msg import Controls
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import PointCloud2
+from sensor_msgs import point_cloud2
 import time
 
 class EKF_SLAM:
@@ -19,9 +20,12 @@ class EKF_SLAM:
         self.mu = np.zeros((3,1)) # px, py, theta
         self.sigma = np.eye(3)*0.01
         self.sigma[2,2] = 0
+    
 
         self.u = np.array([0,0])
-
+        self.zs = None
+        self.n_landmarks = 0
+        self.threshold = 0.5
 
         self.state_pub = rospy.Publisher("/state_marker", Marker, queue_size=1)
         rospy.Subscriber('/controls', Controls, self.state_handle, queue_size=1)
@@ -32,6 +36,17 @@ class EKF_SLAM:
 
     def perception_handle(self, msg: PointCloud2):
         self.prediction_update()
+
+        
+        map = point_cloud2.read_points(msg, field_names=("x", "y", "z","color","score"),skip_nans=True)
+        for c in map:
+            dist = np.sqrt((self.mu[0]+c[0])**2 + (self.mu[1]+c[1])**2)
+            phi = self.mu[2] + np.arctan2(c[1],c[0])
+            self.single_measurement_update(np.array([dist,phi]))
+        
+
+        # self.measurement_update()
+
 
     def prediction_update(self):
 
@@ -55,7 +70,58 @@ class EKF_SLAM:
         G = np.eye(self.sigma.shape[0]) + self.Fx.T @ state_jacobian @ self.Fx # How the model transforms uncertainty
         self.sigma = G @ self.sigma @ G.T + self.Fx.T @ self.R @ self.Fx # Combine model effects and stochastic noise
 
-        print(self.mu)
-        print(self.sigma)
+        # print(self.mu)
+        # print(self.sigma)
+
+    
+
+    def single_measurement_update(self,landmark):
+            dist = landmark[0]
+            phi = landmark[1]
+
+            x = self.mu[0]
+            y = self.mu[1]
+            yaw = self.mu[2]
+
+            mu_landmark = np.array([x + dist*np.cos(phi+yaw),
+                                    y+ dist*np.sin(phi+yaw)]) 
+            
+            d_min = np.Infinity
+            index = None
+            for i in range(self.n_landmarks):
+                d = np.linalg.norm(self.mu[i+3:i+4]-mu_landmark)
+                if d < d_min:
+                    d_min = d
+                    index = i
+                    
+            if d_min < self.threshold:
+                pass 
+            else:
+                self.mu = np.vstack((self.mu, mu_landmark))
+                self.Fx = np.pad(self.Fx, ((0, 0), (0, 2)), 'constant', constant_values=0)
+                self.sigma = np.pad(self.sigma, ((0, 2), (0, 2)), 'constant', constant_values=0)
+                self.sigma[-2,-2] = 0.001
+                self.sigma[-1,-1] = 0.001
+                self.n_landmarks+=1
+                
+            
+            # delta  = mu_landmark - np.array([[x],[y]]) # Helper variable
+            # q = np.linalg.norm(delta)**2 # Helper variable
+
+            # dist_est = np.sqrt(q) # Distance between robot estimate and and landmark estimate, i.e., distance estimate
+            # phi_est = np.arctan2(delta[1,0],delta[0,0])-theta; phi_est = np.arctan2(np.sin(phi_est),np.cos(phi_est)) # Estimated angled between robot heading and landmark
+            # z_est_arr = np.array([[dist_est],[phi_est]]) # Estimated observation, in numpy array
+            # z_act_arr = np.array([[dist],[phi]]) # Actual observation in numpy array
+            # delta_zs[lidx] = z_act_arr-z_est_arr # Difference between actual and estimated observation
+
+            # # Helper matrices in computing the measurement update
+            # Fxj = np.block([[Fx],[np.zeros((2,Fx.shape[1]))]])
+            # Fxj[n_state:n_state+2,n_state+2*lidx:n_state+2*lidx+2] = np.eye(2)
+            # H = np.array([[-delta[0,0]/np.sqrt(q),-delta[1,0]/np.sqrt(q),0,delta[0,0]/np.sqrt(q),delta[1,0]/np.sqrt(q)],\
+            #             [delta[1,0]/q,-delta[0,0]/q,-1,-delta[1,0]/q,+delta[0,0]/q]])
+            # H = H.dot(Fxj)
+            # Hs[lidx] = H # Added to list of matrices
+            # Ks[lidx] = sigma.dot(np.transpose(H)).dot(np.linalg.inv(H.dot(sigma).dot(np.transpose(H)) + Q)) # Add to list of matrices
+
 
 
