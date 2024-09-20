7e7e9f37568cf608bdbcad3e2011b3e11ab6572d
Ángel <angelgares6424@gmail.com>
2024-09-20 20:37:36 +0200

    telemetry launch at boot

--- src/common/common_meta/boot_autonomous_system.sh
+++ src/common/common_meta/boot_autonomous_system.sh
@@ -4,5 +4,7 @@ source /home/arus/workspaces/arus_ws/devel/setup.bash
 # Ejecutar launchs
 sleep 5
 
-roslaunch can_c mission.launch
-rosrun can_c telemetry
+roslaunch can_c mission.launch &
+
+sleep 10
+rosrun can_c telemetry &
