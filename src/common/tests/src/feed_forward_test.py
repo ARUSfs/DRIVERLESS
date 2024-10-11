de4f4bcb0d8298cac53ee93bbfba196704b7104a
alvarolp02 <alplepe02@gmail.com>
2024-10-11 14:37:16 +0200

    feed forward test

--- src/common/tests/src/feed_forward_test.py
+++ src/common/tests/src/feed_forward_test.py
@@ -1,74 +1,70 @@
 #!/usr/bin/env python3
 import rospy
 from common_msgs.msg import Controls, CarState
-from std_msgs.msg import Int16, Bool
+from std_msgs.msg import Int16, Bool, Float32
 import math
 import time
 
+KP = 43.87
+KI = 1.29
 
-PUSHING = False
-DURATION = 27 #s
-
-
-AMPLITUDE = 20 #degrees
-FREQUENCY = 0.2 #s
-KP=0.1
-
-TARGET_SPEED = 1 #m/s
+profile = []
 
 def as_status_callback(msg:Int16):
     global AS_status, start_time
     AS_status = msg.data
     if (AS_status == 2 and start_time == 0):
         start_time=time.time()
-def speed_callback(msg: CarState):
-        global start_time,braking,actual_speed,last_speed_update
+        last_time = time.time()
+
+def speed_callback(msg: Float32):
+        global start_time,braking
 
         if not braking:
             
-            v = math.hypot(msg.vx,msg.vy)
+            v = msg.data
             control_msg = Controls()
 
-            control_msg.steering = generate_sinusoidal_steering(time.time()-start_time)
+            control_msg.steering = 0
 
-            if PUSHING:
-                control_msg.accelerator = 0
-            else:
-                control_msg.accelerator = accelerator_control(v, TARGET_SPEED)
+            
+            control_msg.accelerator = feed_forward(v)
 
             control_publisher.publish(control_msg)
             #rospy.logerr(control_msg)
 
-def generate_sinusoidal_steering(time):
-    steering_angle = AMPLITUDE * math.sin(2*math.pi*FREQUENCY *time)
-#     steering_angle = AMPLITUDE * math.sin(FREQUENCY * time*time)
-    return steering_angle
 
-def accelerator_control(current: float, target: float):
-        global start_time,braking
+def feed_forward(current: float):
+        global start_time,braking,last_time, integral
+
+        target = 2
+        prev_target = 2
+
+        t = time.time()-start_time
+        dt = time.time()-last_time
+        last_time = time.time()
 
         error = target - current
-        cmd = KP*error
+        integral += error*dt
+        cmd_pid = KP*error + KI*integral
 
-        if (time.time()-start_time > DURATION and start_time != 0):
-           braking = True
-           braking_msg = Bool()
-           braking_msg.data = True
-           braking_publisher.publish(braking_msg)
-           rospy.logerr("Frenando")
-
-        return max(min(cmd, 1),-1)
+        target_acc = (target-prev_target)/dt
+        target_acc = target_acc*240*0.2
+        cmd = cmd_pid + target_acc
+        
+        return max(min(cmd/230, 1),-1)
 
 if __name__ == '__main__':
     rospy.init_node('sinusoidal_control_node')
 
     start_time = 0
     AS_status = 0
-
+    last_time = 0
     braking = False
+    integral = 0
 
     control_publisher = rospy.Publisher('/controls_pp', Controls, queue_size=1)
     braking_publisher = rospy.Publisher('/braking', Bool, queue_size=10)
-    rospy.Subscriber('/car_state/state', CarState, speed_callback,queue_size=1)
+    rospy.Subscriber('/wheel_speed', Float32, speed_callback,queue_size=1)
     rospy.Subscriber('/can/AS_status',Int16, as_status_callback, queue_size=1)
     rospy.spin()
